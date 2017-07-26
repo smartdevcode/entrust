d5e658ac6b9f06c5cb125b4510059c0d70d73b66
SeniorDevCode <senior.dev.software@gmail.com>
2017-07-26 02:40:05 +0200

    Add auto discovery

--- src/Entrust/Traits/EntrustUserTrait.php
+++ src/Entrust/Traits/EntrustUserTrait.php
@@ -286,18 +286,4 @@ trait EntrustUserTrait
         }
     }
 
-    /**
-     *Filtering users according to their role 
-     *
-     *@param string $role
-     *@return users collection
-     */
-    public function scopeWithRole($query, $role)
-    {
-        return $query->whereHas('roles', function ($query) use ($role)
-        {
-            $query->where('name', $role);
-        });
-    }
-
 }
